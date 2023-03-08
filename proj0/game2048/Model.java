e26ee34cbac71f7e3070c624011b548b99c7ccde
dys <1115410174@qq.com>
2023-03-08 16:00:14 +0800

    finish proj0 maxTileExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -154,7 +154,13 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for(int i = 0;i < b.size();i++) {
+             for(int j = 0;j < b.size();j++) {
+                 if(b.tile(i,j) != null && b.tile(i,j).value() == MAX_PIECE){
+                     return true;
+                 }
+             }
+        }
         return false;
     }
 
