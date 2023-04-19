e4362608447b63e65c2333c67963f8abbbc5e26d
dys <1115410174@qq.com>
2023-04-19 15:19:08 +0800

    完成了lab3 timeAListConstruction

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,4 +1,5 @@
 package timingtest;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -23,5 +24,20 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList();
+        AList<Double> times = new AList();
+        AList<Integer> opCounts = new AList();
+        for(int i = 1000;i <= 128000;i *= 2){
+           Ns.addLast(i);
+           opCounts.addLast(i);
+           AList<Integer> temp = new AList();
+           Stopwatch sw = new Stopwatch();
+           for(int j = 0;j < i;j++) temp.addLast(j);
+           double timeInSeconds = sw.elapsedTime();
+           times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns,times,opCounts);
+
     }
+
 }
